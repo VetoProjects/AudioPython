5bcca20858a3dca9d357fba70e5ce17974f80838
Veit Heller <veit.heller@student.htw-berlin.de>
2014-08-20 15:26:10 +0200

    Updated setup.py to make pylint happy

--- setup.py
+++ setup.py
@@ -7,18 +7,18 @@ from distutils.core import setup
 import AudioPython
 
 setup(
-    name = 'AudioPython',
-    description = 'An audio live coding library in Python',
-    author = AudioPython.__author__,
-    author_email = AudioPython.__author_mail__,
-    version = AudioPython.__version__,
-    url = AudioPython.__url__,
-    long_description = AudioPython.__longdescr__,
-    classifiers = AudioPython.__classifiers__,
-    keywords = AudioPython.__keywords__,
-    packages = ['AudioPython'],
-    license = "MIT",
-    platforms = ['Linux', 'OS X', 'Windows']
+    name='AudioPython',
+    description='An audio live coding library in Python',
+    author=AudioPython.__author__,
+    author_email=AudioPython.__author_mail__,
+    version=AudioPython.__version__,
+    url=AudioPython.__url__,
+    long_description=AudioPython.__longdescr__,
+    classifiers=AudioPython.__classifiers__,
+    keywords=AudioPython.__keywords__,
+    packages=['AudioPython'],
+    license="MIT",
+    platforms=['Linux', 'OS X', 'Windows']
 )
 
 
